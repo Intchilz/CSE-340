18f5075f48ab7ec3309c324c3497c7cf34c7165b
Intchilz <chi21012@byui.edu>
2026-03-04 22:12:43 +0200

    Completed Week01

--- server.js
+++ server.js
@@ -11,23 +11,27 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-// Index Route
-app.get("/",function (req, res) {
-  res.render("index", {title: "Home"})
-})
-
 /* ***********************
  * View Engine and Templates
  *************************/
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")// not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index Route
+app.get("/",function (req, res) {
+  res.render("index", {title: "Home"})
+})
+
+
+
+
 
 /* ***********************
  * Local Server Information
